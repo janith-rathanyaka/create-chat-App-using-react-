71a578fc53e2c50f1c432ee4f60a4311a0a8d5a9
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2020-12-30 21:36:27 +0530

    update

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -3,11 +3,12 @@ const express = require('express')
 const config = require('./config/app');
 const router = require('./router')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app=express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
+app.use(cors())
 app.use(router)
 
 
@@ -16,7 +17,7 @@ app.use(router)
 //const port = process.env.appPort
 const port=config.appPort;
 app.listen(port,()=>{
-    console.log('Sever listing on port ${port}');
+    console.log(`Sever listing on port ${port}`);
 })
 
 
